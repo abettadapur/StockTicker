9d79861f06ca374b2aeecbe521adc63b97b5d901
Alex Bettadapur <alexbettadapur@gmail.com>
2015-09-07 20:40:34 -0400

    Added flask-restful api endpoint

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from flask_restful import Api
+from rest import api as stock_api
+
 app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(stock_api.HelloWorld, '/api')
 
 @app.route("/")
 def index():
 	return render_template('index.html')
 	
 if __name__ == "__main__":
-	app.run('0.0.0.0', 5000)
+	app.run('0.0.0.0', 5000, debug=True)
