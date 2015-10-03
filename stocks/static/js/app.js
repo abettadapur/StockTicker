1069ccb263983e0f0221d1b0986e7ad8fe01fa68
Alex Bettadapur <alexbettadapur@gmail.com>
2015-10-03 15:12:45 -0400

    Updated website to use new angular routing

--- stocks/static/js/app.js
+++ stocks/static/js/app.js
@@ -1,11 +1,4 @@
-
 'use strict';
-var StockTrackerApp = angular.module('StockTrackerApp', []);
 
-StockTrackerApp.controller('StockTrackerCtrl1', function($scope, $http) {
-    $http.get('/api/stocks/AAPL/report')
-        .success(function (response) {
-            $scope.particularStock =
-                response;
-        });
-});
+
+
--- stocks/static/js/controllers.js
+++ stocks/static/js/controllers.js
@@ -1,3 +1,17 @@
-/**
- * Created by Andrew Hull on 9/21/15.
- */
+'use strict';
+var stockApp = angular.module('StockTrackerApp', ['ngRoute']);
+
+stockApp.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'static/pages/home.html',
+        controller: 'homeController'
+    });
+});
+
+stockApp.controller('homeController', function ($scope, $http) {
+    $http.get('/api/stocks/AAPL/report')
+        .success(function (response) {
+            $scope.particularStock =
+                response;
+        });
+});
