0e597ce772014110e08f64d711e8617d7ba25e2e
Alex Bettadapur <alexbettadapur@gmail.com>
2015-10-04 16:21:12 -0400

    Added charting

--- stocks/static/js/controllers.js
+++ stocks/static/js/controllers.js
@@ -7,18 +7,51 @@ function HomeController ($scope, StockReport)
 
 function ChartController($scope, StockReport)
 {
-    var historyQuery = StockReport.GetChart({ stock: "AAPL", from: "2015-01-01", to: "2015-10-04" }, function (points) {
-        Morris.Line({
-            element: "morris-area-chart",
-            data: points,
-            xkey: 'Date',
-            ykeys: ['Close'],
-            labels: ['Price'],
-            pointSize: 0,
-            hideHover:'auto',
-            resize: true
+    $('#frompicker').datetimepicker(
+        {
+            format: 'YYYY-MM-DD'
         });
+    $('#topicker').datetimepicker(
+        {
+            format: 'YYYY-MM-DD',
+            useCurrent: false
+        });
+
+    $('#frompicker').on("dp.change", function (e) {
+        $('#topicker').data("DateTimePicker").minDate(e.date);
     });
+
+    $('#topicker').on("dp.change", function (e) {
+        $('#frompicker').data("DateTimePicker").maxDate(e.date);
+    });
+
+    $scope.chartHeading = ""
+    $scope.loading = false;
+
+    $scope.loadChart = function (symbol) {
+        $scope.loading = true;
+        $scope.chartHeading = "Loading...";
+        fromdate = $("#frompicker").data("date");
+        todate = $("#topicker").data("date");
+
+        $("#morris-area-chart").empty();
+        var historyQuery = StockReport.GetChart({ stock: symbol.toUpperCase(), from: fromdate, to: todate }, function (points) {
+            Morris.Line({
+                element: "morris-area-chart",
+                data: points,
+                xkey: 'Date',
+                ykeys: ['Close'],
+                labels: ['Price'],
+                pointSize: 0,
+                hideHover: 'auto',
+                resize: true
+            });
+            $scope.chartHeading = "Historical data for " + symbol.toUpperCase();
+            $scope.loading = false;
+        });
+        
+    };
+    
     
 }
 
